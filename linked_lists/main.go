22689de132d845ca1850597bc5c3c8b41f1173f1
David Webster <dwebster@appacademy.io>
2018-05-09 21:07:01 -0700

    Finish queue implemented with linked lists

--- linked_lists/main.go
+++ linked_lists/main.go
@@ -4,25 +4,148 @@ import (
   "fmt"
 )
 
+// STRUCT DEFINITIONS //////
+
 type node struct {
   data int
   next *node
 }
 
-type list struct {
-  head *node
-  length int
+type linkedList struct {
+  head *node 
+  length int 
 }
 
-// func (l list) append(n *node) {
-//
+type queue struct {
+  inStack *linkedList
+  outStack *linkedList
+}
+
+// QUEUE METHODS /////
+
+func (q *queue) push(val int) {
+  if q.inStack == nil {
+    q.inStack = &linkedList{}
+  }
+
+  q.inStack.insert(val)
+}
+
+func newQueue() queue {
+  q := queue{}
+  q.inStack, q.outStack = &linkedList{}, &linkedList{}
+
+  return q
+}
+
+func (q *queue) pop() int {
+  if q.outStack == nil {
+    q.outStack = &linkedList{}
+  }
+
+  if q.outStack.isEmpty() {
+    q.flip()
+    return q.outStack.pop()
+  } else {
+    return q.outStack.pop()
+  }
+}
+
+func (q *queue) flip() {
+  for !q.inStack.isEmpty() {
+    q.outStack.insert(q.inStack.pop())
+  }
+}
+
+// func (q *queue) peek() int {
+//   var topVal int
+
+//   if q.outStack == nil && q.inStack == nil {
+//     panic("The queue is empty")
+//   } else if q.outStack.isEmpty() && q.inStack.isEmpty() {
+//     panic("The queue is empty")
+//   } else if q.outStack.isEmpty() {
+//     current := q.inStack.head 
+
+//     for current.next != nil {
+//       current = current.next
+//     }
+
+//     topVal = current.data
+//   } else {
+//     topVal = q.outStack.head.data
+//   }
+
+//   return topVal
 // }
-//
-// func (l list) pop() *node {
-//
-// }
+
+// LINKED LIST METHODS //////
+
+func (l *linkedList) insert(val int) {
+  n := node{data: val}
+  n.next = l.head
+  l.head = &n
+
+  l.length++
+}
+
+func (l linkedList) isEmpty() bool {
+  return l.head == nil
+}
+
+func (l *linkedList) pop() int {
+  l.length--
+  poppedNode := l.head
+  l.head = poppedNode.next 
+  poppedNode.next = nil
+
+  return poppedNode.data
+}
 
 
 func main() {
-  fmt.Println("Hello, World!")
+  // list := linkedList{}
+  
+  // fmt.Printf("Right now, our list looks like %T\n", list)
+
+  // list.insert(1)
+  // list.insert(2)
+  // list.insert(3)
+  // list.insert(4)
+  // list.insert(5)
+
+  // fmt.Printf("The head node has a value %v\n", list.head.data)
+
+  // current := list.head
+
+  // for current.next != nil {
+  //   current = current.next
+  // }
+
+  // fmt.Printf("The tail of the linked list is %v\n", current.data)
+
+  // fmt.Printf("The head node has a value %v\n", list.head.data)
+  // fmt.Printf("Right now our list has a length of %v\n", list.length)
+  // fmt.Printf("When I pop the list, the value we get is %v\n", list.pop())
+
+  // myQueue := queue{}
+  // myQueue.push(5)
+  // myQueue.push(6)
+  
+  // myQueue.peek()
+  // list := linkedList{}
+
+  // list.insert(5)
+    myQueue := newQueue()
+    myQueue.push(1)
+    myQueue.push(2)
+    myQueue.push(3)
+    fmt.Printf("The last value is %v\n", myQueue.pop())
+    // fmt.Printf("The value is %#v\n", myQueue.inStack.head)
+    
+  // myQueue.push(1)
+  // myQueue.push(2)
+  // myQueue.push(3)
+
+  // fmt.Printf("The top of the ")
 }
--- my_go_notes/main.go
+++ my_go_notes/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func modifyInteger(arg *int) {
-  // NB: "*" in params tells the function to create a pointer to its memory location. Without it we cannot mutate what was given
+  // NOTE: "*" in params tells the function to create a pointer to its memory location. Without it we cannot mutate what was given
 
   // intVal := *ptrToInt
 
@@ -35,5 +35,25 @@ NOTE:
 
 Objects passed into functions are not mutable in Go
 
+In order to pass in an object by reference, you must:
+  pass it in like this: methodName(&object)
+  have the method accept the object func growOneYear(c *cat) {}
 
+
+In go, you can have methods on things that are not structs
+
+You cannot define methods on built in Go types - you can make an alias for any type, however
+  type nedInt int
+    --> this makes an integer-like object (basically an alias)
+
+array syntax
+  myNums := [2]int{}
+
+Casting:
+  if you have a stack of objects that use the anything interface:
+    var a2 anything
+    var num2 int
+
+    a2 = stack.pop()
+    num2 = a2.(int) <-- castings
 */
